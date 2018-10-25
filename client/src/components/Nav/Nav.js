8e2448e8d8911a6835949f7b1c023e43abb3803d
asif <asif.vora@multidots.com>
2018-10-25 16:08:46 +0530

    adding bioostrap.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
 	<nav className="navbar navbar-inverse navbar-top">
 		<div className="container-fluid">
 			<div className="navbar-header">				
-				<Link className="navbar-brand" to="/">Home</Link>
+				<Link className="navbar-brand" to="/">Book Shop</Link>
 			</div>
 		</div>
 	</nav>
